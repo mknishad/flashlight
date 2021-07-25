9a252d60848707b883ddc4cd28244faf2f9a61dc
mknishad <mknishad1@ymail.com>
2021-07-26 03:58:40 +0600

    dont show full screen

--- app/src/main/java/com/ideafactorybd/flashlight/MainActivity.java
+++ app/src/main/java/com/ideafactorybd/flashlight/MainActivity.java
@@ -26,9 +26,9 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     // remove title
-    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    /*requestWindowFeature(Window.FEATURE_NO_TITLE);
     getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
     setContentView(R.layout.activity_main);
 
     boolean isFlashAvailable = getApplicationContext().getPackageManager()
