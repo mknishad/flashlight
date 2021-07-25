9aa3eb326630028959ae4b6dbba73a2150df07f4
mknishad <mknishad1@ymail.com>
2021-07-26 03:08:41 +0600

    remove ads

--- app/src/main/java/com/ideafactorybd/nishad/flashlight/MainActivity.java
+++ app/src/main/java/com/ideafactorybd/nishad/flashlight/MainActivity.java
@@ -11,107 +11,75 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton imageButton;
-    private Camera camera;
-    private Camera.Parameters parameters;
-    private boolean isFlash = false;
-    private boolean isOn = false;
-    private AdView mAdView;
+  private ImageButton imageButton;
+  private Camera camera;
+  private Camera.Parameters parameters;
+  private boolean isFlash = false;
+  private boolean isOn = false;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // remove title
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // remove title
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    setContentView(R.layout.activity_main);
 
-        mAdView = (AdView) findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder()
-                .build();
-        mAdView.loadAd(adRequest);
+    imageButton = (ImageButton) findViewById(R.id.imageButton);
 
-        imageButton = (ImageButton) findViewById(R.id.imageButton);
+    if (getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+      camera = Camera.open();
+      parameters = camera.getParameters();
+      isFlash = true;
+    }
 
-        if(getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
-            camera = Camera.open();
-            parameters = camera.getParameters();
-            isFlash = true;
-        }
-
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    if (isFlash) {
-                        if (!isOn) {
-                            imageButton.setImageResource(R.drawable.on);
-                            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-                            camera.setParameters(parameters);
-                            camera.startPreview();
-                            isOn = true;
-                        } else {
-                            imageButton.setImageResource(R.drawable.off);
-                            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-                            camera.setParameters(parameters);
-                            camera.stopPreview();
-                            isOn = false;
-                        }
-                    } else {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                        builder.setTitle("Error...");
-                        builder.setMessage("Flashlight is not available on this device...");
-                        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                                finish();
-                            }
-                        });
-                        AlertDialog alertDialog = builder.create();
-                        alertDialog.show();
-                    }
-                } catch (Exception e) {}
+    imageButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        try {
+          if (isFlash) {
+            if (!isOn) {
+              imageButton.setImageResource(R.drawable.on);
+              parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+              camera.setParameters(parameters);
+              camera.startPreview();
+              isOn = true;
+            } else {
+              imageButton.setImageResource(R.drawable.off);
+              parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+              camera.setParameters(parameters);
+              camera.stopPreview();
+              isOn = false;
             }
-        });
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (camera != null) {
-            camera.release();
-            camera = null;
+          } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Error...");
+            builder.setMessage("Flashlight is not available on this device...");
+            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                finish();
+              }
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+          }
+        } catch (Exception e) {
         }
-    }
+      }
+    });
+  }
 
-    @Override
-    public void onPause() {
-        if (mAdView != null) {
-            mAdView.pause();
-        }
-        super.onPause();
+  @Override
+  protected void onStop() {
+    super.onStop();
+    if (camera != null) {
+      camera.release();
+      camera = null;
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (mAdView != null) {
-            mAdView.resume();
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        if (mAdView != null) {
-            mAdView.destroy();
-        }
-        super.onDestroy();
-    }
+  }
 }
